9c5143b1c0a6cea7d112525f4b1fa54bb8e65558
alejandro <alejandro.minambres@gmail.com>
2024-03-28 19:24:03 +0100

    clase mensaje protocolo terminada

--- src/sdis/spotify/common/MensajeProtocolo.java
+++ src/sdis/spotify/common/MensajeProtocolo.java
@@ -10,8 +10,8 @@ public class MensajeProtocolo implements java.io.Serializable {
      * @param p, tipo de primitiva
      * @param mensaje, mensaje que contendrá usuario y contraseña.
      */
-    public MensajeProtocolo(Primitiva p, String mensaje, String idCola) throws MalMensajeProtocoloException {
-            if (p == Primitiva.XAUTH){
+    public MensajeProtocolo(Primitiva p,String idCola, String mensaje ) throws MalMensajeProtocoloException {
+            if (p == Primitiva.XAUTH || p== Primitiva.ADD2L){
                 this.primitiva = p;
                 this.mensaje = mensaje;
                 this.idCola = idCola;
@@ -20,37 +20,37 @@ public class MensajeProtocolo implements java.io.Serializable {
     }
 
     /**
-     * Constructor para ADDED e INFO
+     * Constructor para ADDED, EMPTY, DELETED
      * @param p
      * @throws MalMensajeProtocoloException
      */
     public MensajeProtocolo(Primitiva p) throws MalMensajeProtocoloException {
-        if (p  == Primitiva.ERROR || p == Primitiva.ADDED) {
-        this.primitiva = p;
-        this.mensaje = this.idCola = null;
-        }
-        else if (p == Primitiva.INFO){
+        if (p == Primitiva.ADDED || p == Primitiva.EMPTY || p == Primitiva.DELETED ) {
             this.primitiva = p;
-            this.mensaje = "Welcome, please type your credentials to LOG in";
-            this.idCola = null;
+            this.mensaje = this.idCola = null;
         }
         else throw new MalMensajeProtocoloException();
     }
 
     /**
-     * Constructor para NOTAUTH ,ERROR y XAUTH confirmación
+     * Constructor para INFO, READL, MEDIA, DELETEL, NOTAUTH ,ERROR y XAUTH confirmación
      * @param p
      * @param mensaje
      * @throws MalMensajeProtocoloException
      */
     public MensajeProtocolo(Primitiva p, String mensaje) throws MalMensajeProtocoloException{
-        if (p == Primitiva.NOTAUTH || p == Primitiva.ERROR || p == Primitiva.XAUTH){
+        if (p== Primitiva.INFO|| p == Primitiva.XAUTH || p == Primitiva.MEDIA || p == Primitiva.NOTAUTH || p == Primitiva.ERROR ){
             this.primitiva = p;
             this.mensaje = mensaje;
             this.idCola = null;
-        } 
+        } else if(p == Primitiva.READL || p == Primitiva.DELETEL){
+            this.primitiva = p;
+            this.mensaje = null;
+            this.idCola = mensaje;
+        }
         else throw new MalMensajeProtocoloException();
     }
+
 
 
     public Primitiva getPrimitiva() { return this.primitiva; }
@@ -62,9 +62,16 @@ public class MensajeProtocolo implements java.io.Serializable {
     public String toString() {  //prettyPrinter de la clase
         switch (this.primitiva) {
             case XAUTH:
+            case INFO:
+            case MEDIA:
+            case NOTAUTH:
+            case ERROR:
                 return this.primitiva+":"+this.mensaje ;
             case ADD2L:
                 return this.primitiva+":"+this.idCola+":"+this.mensaje;
+            case READL:
+            case DELETEL:
+                return this.primitiva+":"+this.idCola;
             default :
         return this.primitiva.toString() ;
     }
